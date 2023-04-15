b5397bc627b4b019b2534c9651282fbec88c2fb8
Mayank Tripathi <tripathimayank216@gmail.com>
2023-04-15 10:18:25 +0530

    Update index, footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     const date = new Date();;
     const year = date.getFullYear();
-    return <footer><p>Copyright © {year}</p></footer>;
+    return <footer><p>Copyright © {year} Mayank Tripathi</p></footer>;
 }
 
 export default Footer;
