edc7d1c63627befb1d719553456318331e63e5dd
Mayank Tripathi <tripathimayank216@gmail.com>
2023-05-09 19:57:46 +0530

    fix navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,30 +11,32 @@ function Navbar() {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light">
+
           <Link class="navbar-brand" to="/">
             <img src="../keepit.png" alt="..." height="50" />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/"><h3>Home</h3></Link>
+                <Link class="nav-link active" to="/"><h3>Home</h3></Link>
               </li>
-              {!localStorage.getItem("token") ? <form className="d-flex" role="search">
+              {!localStorage.getItem("token") ? <>
                 <li className="nav-item">
                   <Link class="nav-link" to="/login"><h3>Login</h3></Link>
                 </li>
                 <li className="nav-item">
                   <Link class="nav-link" to="/register"><h3>Register</h3></Link>
                 </li>
-              </form> : <li className="nav-item"><Link class="nav-link" onClick={handleLogout}><h3>Logout</h3></Link></li>}
+              </> : <li className="nav-item"><Link class="nav-link" onClick={handleLogout}><h3>Logout</h3></Link></li>}
             </ul>
           </div>
-        </div>
+
       </nav>
     </div>
   )
